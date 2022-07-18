55d2bb4af9d8654dd781f6337d092d222c2dda9f
kunjan125323 <107481239+kunjan125323@users.noreply.github.com>
2022-07-18 18:03:30 +0530

    Add files via upload

---
+++ js/demo.js
@@ -0,0 +1,16 @@
+function demoexternalalert(){
+    alert("external alert");
+}
+function demoexternalconfirm(){
+    if(confirm("are you sure...??")){
+        alert("yesss");
+    }
+    else{
+        alert("nooo");
+    }
+}
+function demoexternalalpromot(){
+    varfname=prompt("enter firstname here..");
+    varfname=promot("enter lastname here..");
+    alert(fname+""+lname);
+}
